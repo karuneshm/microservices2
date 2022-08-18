031bf00ab662876e1a8ac7839461456ab166bc47
karuneshm <karunesh@gmail.com>
2022-08-18 15:26:39 +0530

    micro services

--- currency-exchange-service-2/src/main/java/com/karunesh/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service-2/src/main/java/com/karunesh/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -4,6 +4,8 @@ package com.karunesh.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	@Autowired
 	private Environment environment;
@@ -26,6 +30,8 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retriveExchangedValues(@PathVariable(value = "from") String from, 
 			@PathVariable(value = "to") String to) {
 		//CurrencyExchange currencyExchange = new CurrencyExchange(1000l,from,to, BigDecimal.valueOf(65));
+		
+		logger.info(" retriveExchangedValues called with {} to {}", from,to);
 		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if(null == currencyExchange) {
