ae55fcabd469b029e12e04d85694da593d161b7b
karuneshm <karunesh@gmail.com>
2022-08-18 14:37:08 +0530

    micro services

---
+++ limits-service-2/src/test/java/com/karunesh/microservices/limitsservice2/LimitsService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karunesh.microservices.limitsservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
